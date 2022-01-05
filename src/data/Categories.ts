4133a76eb7d8afed3054a02ff093cf7d536ab904
Guilherme Coelho Vieira <80927565+Wookyse@users.noreply.github.com>
2022-01-05 15:01:02 -0300

    Add files via upload

--- src/data/Categories.ts
+++ src/data/Categories.ts
@@ -1,8 +1,11 @@
 import { Category } from '../types/Category'
 
 export const Categories: Category = {
-  buy: { title: 'Compras', color: 'red', expense: true },
-  food: { title: 'Comida', color: 'blue', expense: true },
+  salary2: { title: 'Extra/Comissão', color: 'green', expense: false },
+  salary: { title: 'Salário', color: 'green', expense: false },
   rent: { title: 'Aluguel', color: 'brown', expense: true },
-  salary: { title: 'Salário', color: 'green', expense: false }
+  food: { title: 'Comida', color: 'blue', expense: true },
+  buy: { title: 'Compras', color: 'orange', expense: true },
+  card: { title: 'Fatura Cartão', color: 'red', expense: true },
+  billsToPay: { title: 'Contas a Pagar', color: 'blue', expense: true }
 }
--- src/data/Items.ts
+++ src/data/Items.ts
@@ -1,34 +1,34 @@
 import { Item } from '../types/Item'
 
 export const Items: Item[] = [
-  {
-    date: new Date(2021, 11, 15),
-    category: 'food',
-    title: 'McDonalds',
-    value: 32.12
-  },
-  {
-    date: new Date(2021, 11, 6),
-    category: 'food',
-    title: 'BurguerKing',
-    value: 28
-  },
-  {
-    date: new Date(2021, 11, 15),
-    category: 'rent',
-    title: 'Aluguel',
-    value: 500
-  },
-  {
-    date: new Date(2022, 0, 5),
-    category: 'salary',
-    title: 'Salário',
-    value: 800
-  },
-  {
-    date: new Date(2021, 11, 5),
-    category: 'salary',
-    title: 'Salário',
-    value: 800
-  }
+  // {
+  //   date: new Date(2021, 11, 15),
+  //   category: 'food',
+  //   title: 'McDonalds',
+  //   value: 32.12
+  // },
+  // {
+  //   date: new Date(2021, 11, 6),
+  //   category: 'food',
+  //   title: 'BurguerKing',
+  //   value: 28
+  // },
+  // {
+  //   date: new Date(2021, 11, 15),
+  //   category: 'rent',
+  //   title: 'Aluguel',
+  //   value: 500
+  // },
+  // {
+  //   date: new Date(2022, 0, 5),
+  //   category: 'salary',
+  //   title: 'Salário',
+  //   value: 800
+  // },
+  // {
+  //   date: new Date(2021, 11, 5),
+  //   category: 'salary',
+  //   title: 'Salário',
+  //   value: 800
+  // }
 ]
